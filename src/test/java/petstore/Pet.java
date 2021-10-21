c9137efe30acfa3139836e99f92c3a7d9d1869dc
elayneap <elayneparaiba@gmail.com>
2021-10-20 21:45:14 -0300

    Criação do teste ExcluirPet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -82,4 +82,22 @@ public class Pet {
                 .body("status", is("donated"))
         ;
     }
+
+    @Test(priority = 4)
+    public void ExcluirPet(){
+        String petId = "1974080123";
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code",is(200))
+                .body("type",is("unknown"))
+                .body("message",is(petId))
+        ;
+    }
 }
